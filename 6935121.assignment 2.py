e4b719e1ccf5b5bb32fea498163d2f7280c46412
PearlDogli <125612300+PearlDogli@users.noreply.github.com>
2023-02-24 11:08:40 +0000

    Add files via upload

---
+++ 6935121.assignment 2.py
@@ -0,0 +1,47 @@
+#varieties of cars and the cost involved
+#check if car brand is in the list
+carbrand = input("Please enter the car brand: ")
+if carbrand == "Tesla":
+    print('Yes and the price is USD 9 million')
+elif carbrand == "Bugatti":
+    print('Yes and the price is USD 7000')
+elif carbrand == "Ferrari":
+    print('Yes and the price is USD 56000')
+elif carbrand == "Porche":
+    print('Yes and the price is USD 6000')
+elif carbrand == "V8":
+    print('Yes and the price is USD 4000')
+elif carbrand == "Range Rover":
+    print('Yes and the price is USD 10000')
+elif carbrand == "Kia":
+    print('Yes and the price is USD 3000')
+elif carbrand == "Hyundai":
+    print('Yes and the price is USD 5000')
+elif carbrand == "Jaguar":
+    print('Yes and the price is USD 4500')
+elif carbrand == "Honda civic":
+    print('Yes and the price is USD 75000')
+elif carbrand == "Opel":
+    print('Yes and the price is USD 63000')
+elif carbrand == "Ford":
+    print('Yes and the price is USD 72000')
+elif carbrand == " Mercedes Benz":
+    print('Yes and the price is USD 63000')
+elif carbrand == "BMW":
+    print('Yes and the price is USD 82000')
+elif carbrand == "Nissan":
+    print('Yes and the price is USD 67000')
+elif carbrand == "Corolla":
+    print ('Yes and the price is USD 79000')
+elif carbrand == "Lexus":
+    print('Yes and the price is USD 84000')
+elif carbrand == "Bentley":
+    print('Yes and the price is USD 51000')
+elif carbrand == "Lamborghini":
+    print('Yes and the price is USD 400000')
+elif carbrand == "Audi":
+    print('Yes and the price is USD 95000')
+else:
+    print('No') 
+#https://github.com/PearlDogli/6935121.car-dealer-assignment-2.git
+    
